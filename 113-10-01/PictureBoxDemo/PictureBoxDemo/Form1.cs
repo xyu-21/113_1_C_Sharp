df4ae4f93e6dbb37f8c85a546b0a100c70b0a5cf
xyu-21 <A111221046@mail.shu.edu.tw>
2024-10-08 16:02:32 +0800

    113-10-08

---
+++ 113-10-01/PictureBoxDemo/PictureBoxDemo/Form1.cs
@@ -0,0 +1,20 @@
+namespace PictureBoxDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("好想坐熱氣球");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
