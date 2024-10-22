3746fdbd973dfb13faeb27989412f1a156649a06
xyu-21 <A111221046@mail.shu.edu.tw>
2024-10-22 18:02:57 +0800

    113-10-22

---
+++ 113-10-22/Tutorial 3-4/Test Average/Test Average/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_Average
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            decimal test1;
+            decimal test2;
+            decimal test3;
+            decimal average;
+            try
+            {
+                test1 = decimal.Parse(test1TextBox.Text);
+                test2 = decimal.Parse(test2TextBox.Text);
+                test3 = decimal.Parse(test3TextBox.Text);
+                average = (test1 + test2 + test3)/3;
+
+                averageLabel.Text = average.ToString("n");
+            }
+            catch 
+            {
+                MessageBox.Show("輸入資料格式錯誤");
+                test1TextBox.Text = "";
+                test2TextBox.Text = "";
+                test3TextBox.Text = "";
+                test1TextBox.Focus();
+            }
+
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            test1TextBox.Text = "";
+            test2TextBox.Text = "";
+            test3TextBox.Text = "";
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();   
+        }
+    }
+}
---
+++ 113-10-22/Tutorial 3-5/Change Counter/Change Counter/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Change_Counter
+{
+    public partial class Form1 : Form
+    {
+        const decimal FIVE_CENYS_VALUE = 0.05m;
+        const decimal TEN_CENYS_VALUE = 0.10m;
+        const decimal TWENTYFIVE_CENYS_VALUE = 0.25m;
+        const decimal FIFTY_CENYS_VALUE = 0.50m;
+        const decimal TOTAL = 0.00m;
+        private decimal total ;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void fiveCentsPictureBox_Click(object sender, EventArgs e)
+        {
+            //total = total + 0.05m;            
+            total += FIVE_CENYS_VALUE;
+            totalLabel.Text = total.ToString("C");
+        }
+
+        private void tenCentsPictureBox_Click(object sender, EventArgs e)
+        {
+            total += TEN_CENYS_VALUE;
+            totalLabel.Text = total.ToString("C");
+        }
+
+        private void twentyFiveCentsPictureBox_Click(object sender, EventArgs e)
+        {
+            total += TWENTYFIVE_CENYS_VALUE;
+            totalLabel.Text = total.ToString("C");
+        }
+
+        private void fiftyCentsPictureBox_Click(object sender, EventArgs e)
+        {
+            total += FIFTY_CENYS_VALUE;
+            totalLabel.Text = total.ToString("C");
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            total = TOTAL;
+            totalLabel.Text = total.ToString("C");
+        }
+    }
+}
