2d7968ee288a584ae57cf06922b37ec7b9f48860
xyu-21 <A111221046@mail.shu.edu.tw>
2024-10-29 18:00:40 +0800

    113-10-29

---
+++ 113-10-29/Tutorial 4-2/Payroll with Overtime/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Payroll_with_Overtime
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            decimal hoursWorked, hourlyPayRate, grossPay;
+            const decimal BASE_HOURS = 40;
+            const decimal OVERTIME_RATE = 1.5m;
+            
+            try
+            {
+                hoursWorked = decimal.Parse(hoursWorkedTextBox.Text);
+                hourlyPayRate = decimal.Parse(hourlyPayRateTextBox.Text);
+
+                if (hoursWorked > BASE_HOURS)
+                {
+                    grossPay = (BASE_HOURS * hourlyPayRate) + ((hoursWorked - BASE_HOURS) * hourlyPayRate * OVERTIME_RATE);
+                }
+                else 
+                {
+                    grossPay = hoursWorked*hourlyPayRate;
+                }
+
+                grossPayLabel.Text = grossPay.ToString("c");
+            }
+            catch (Exception ex)
+            {
+                //Display an error message.
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // Clear the TextBoxes and gross pay label.
+            hoursWorkedTextBox.Text = "";
+            hourlyPayRateTextBox.Text = "";
+            grossPayLabel.Text = "";
+
+            // Reset the focus.
+            hoursWorkedTextBox.Focus();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
