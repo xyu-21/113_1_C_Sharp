af81cd061f8689f0ab39a3bc0dc09bc194f960ff
xyu-21 <A111221046@mail.shu.edu.tw>
2024-11-19 18:10:41 +0800

    113-11-19

---
+++ 113-11-19/Tutorial 4-5/Color Theme/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Color_Theme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void yellowRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void redRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void whiteRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void normalRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string res = "";
+            if (checkBox1.Checked) 
+            {
+                res += "You checked the box1.\n";
+                MessageBox.Show(res);
+            }
+            if (checkBox2.Checked)
+            {
+                res += "You checked the box2.\n";
+                MessageBox.Show(res);
+            }
+            if (checkBox3.Checked)
+            {
+                res += "You checked the box3.\n";
+                MessageBox.Show(res);
+            }
+        }
+    }
+}
---
+++ 113-11-19/Tutorial 4-6/Time Zone/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Time_Zone
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            city = cityListBox.SelectedItem.ToString();
+
+            switch (city)
+           { 
+
+           case "Denver":
+                timeZoneLabel.Text = "Mountain";
+                break;
+           case "Honolulu":
+                timeZoneLabel.Text = "Hawaii-Aleytain";
+                break;
+           case "Minneapolis":
+                timeZoneLabel.Text = "Central";
+           case "San Franciso":
+                timeZoneLabel.Text = "Mountain";
+                break;
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
