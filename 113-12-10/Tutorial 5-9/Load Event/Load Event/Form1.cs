b13704b4e52721cfa1e35cba56477aaab955352a
xyu-21 <A111221046@mail.shu.edu.tw>
2024-12-10 18:00:49 +0800

    113-12-10

---
+++ 113-12-10/Tutorial 5-9/Load Event/Load Event/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Load_Event
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            exitButton.Text = "離開";
+            try
+            {
+                StreamReader inputFile; //用來讀取檔案的物件
+                string countryName;     //用來保存檔案中的資料
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+                    inputFile = File.OpenText(openFile.FileName); //開啟檔案
+                    countriesListBox.Items.Clear();
+                    while (!inputFile.EndOfStream) //當未到達檔案結尾時
+                    {
+                        countryName = inputFile.ReadLine(); //讀取一行資料
+                        countriesListBox.Items.Add(countryName); //將資料加入ListBox
+                    }
+                    inputFile.Close(); //關閉檔案
+                }
+                else
+                {
+                    MessageBox.Show("未選擇檔案!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+
+        private void countriesListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show(countriesListBox.SelectedItem.ToString());
+        }
+    }
+}
---
+++ 113-12-10/Tutorial 6-1/Lights/Lights/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lights
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void turnLightOn() 
+        {
+            lightOffPictureBox.Visible = false;
+            lightOnPictureBox.Visible = true;
+            lightStateLabel.Text = "ON";
+        } 
+        private void turnLightOff() 
+        {
+            lightOffPictureBox.Visible = true;
+            lightOnPictureBox.Visible = false;
+            lightStateLabel.Text = "OFF";
+        }
+
+        private void switchButton_Click(object sender, EventArgs e)
+        {
+            if(lightOnPictureBox.Visible == true)
+            {
+                //關燈
+                turnLightOff();
+            }
+            else 
+            {
+                //開燈
+                turnLightOn();
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
